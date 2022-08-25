28236655876806f6a0a4361489c93ac50decfbf0
littlekillerkalf <thomaskalf@gmail.com>
2022-08-25 23:45:46 +1000

    Start and Pause

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject mainUI;
+    public void StartGame()
+    {
+        mainMenu.SetActive(false);
+        mainUI.SetActive(true);
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            PauseGame();
+        }
+    }
+    public void PauseGame()
+    {
+        mainMenu.SetActive(true);
+        mainUI.SetActive(false);
+    }
+}
