835d029d02246ad16768df58750990fa277e9366
littlekillerkalf <thomaskalf@gmail.com>
2022-08-26 00:10:50 +1000

    Audio for Cupboard

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,21 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenu : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject mainUI;
+    public AudioSource cupboard;
+    private bool mainMenuOpen = true;
     public void StartGame()
     {
         mainMenu.SetActive(false);
         mainUI.SetActive(true);
         Debug.Log("can press Button");
+        mainMenuOpen = false;
     }
     public void Update()
     {
-        if (Input.GetKeyDown("p"))
+        if (!mainMenuOpen)
         {
-            PauseGame();
+            if (Input.GetKeyDown("p"))
+            {
+                PauseGame();
+                mainMenuOpen = true;
+            }
+            if (Input.GetKeyDown("e"))
+            {
+                cupboard.Play();
+            }
         }
     }
     public void PauseGame()
